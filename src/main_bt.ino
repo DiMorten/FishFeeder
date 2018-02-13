1cd0093c15af896bee305fb4410d19f83a2bf35b
Jorge Andres Chamorro Martinez <cmjorgeandres@gmail.com>
2018-02-13 16:43:56 -0500

    First

---
+++ src/main_bt.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
+SoftwareSerial BT(10,11);    // Definimos los pines RX y TX del Arduino conectados al Bluetooth
+ 
+void setup()
+{
+  BT.begin(9600);       // Inicializamos el puerto serie BT que hemos creado
+  Serial.begin(9600);   // Inicializamos  el puerto serie  
+}
+
+const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+void loop()
+{
+  if(BT.available())    // Si llega un dato por el puerto BT se envía al monitor serial
+  {
+    Serial.write(BT.read());
+    bt_recvWithEndMarker();
+  }
+
+  if(Serial.available())  // Si llega un dato por el monitor serial se envía al puerto BT
+  {
+     BT.write(Serial.read());
+  }
+}
+
+
+void bt_recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+   
+    while (BT.available() > 0 && newData == false) {
+        rc = BT.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+        newData = false;
+    }
+}
